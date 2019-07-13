Fixed bug in area computation where there are lost ensembles. Fixed bug flipping the sign of GPS data based on E/W or N/S geographic coordinates Modified abba_2d_interpolation to check if any valid data are available for interpolation Modified Qtmpl extrap_plot to handle data where there are no valid water data.

Classes/Measurement.py
@@ -559,8 +559,6 @@ class Measurement(object):
                 self.discharge.append(q)
 
 
-
-
     @staticmethod
     def set_num_beam_wt_threshold_trdi(mmt_transect):
         """Get number of beams to use in processing for WT from mmt file
@@ -1450,10 +1448,11 @@ class Measurement(object):
 
                 # Get selected depth object
                 depth = getattr(transect.depths, transect.depths.selected)
-
+                depth_a = np.copy(depth.depth_processed_m)
+                depth_a[np.isnan(depth_a)] = 0
                 # Compute area of the moving-boat portion of the cross section using trapezoidal integration.
                 # This method is consistent with AreaComp but is different from QRev in Matlab
-                area_moving_boat = np.abs(np.trapz(depth.depth_processed_m[in_transect_idx], station[in_transect_idx]))
+                area_moving_boat = np.abs(np.trapz(depth_a[in_transect_idx], station[in_transect_idx]))
 
                 # Compute area of left edge
                 edge_idx = QComp.edge_ensembles('left', transect)

Classes/QAData.py
@@ -653,7 +653,7 @@ class QAData(object):
                     self.moving_bed['code'] = 1
 
                     # Check if there is a moving-bed
-                    if any(mb):
+                    if 'Yes' in mb:
                         # Moving-bed present
                         self.moving_bed['messages'].append(
                             ['Moving-Bed Test: A moving-bed is present, use GPS or moving-bed correction;', 2, 6])
@@ -1115,7 +1115,7 @@ class QAData(object):
 
             # Generate message for all invalid
             if np.any(self.w_vel['all_invalid']):
-                self.w_vel['messages'].append(['WT-', prefix[0], 'There are no valid data for one or more transects.',
+                self.w_vel['messages'].append(['WT-' + prefix[0] + 'There are no valid data for one or more transects.',
                                                1, 11])
                 status_switch = 2
 
@@ -1338,7 +1338,7 @@ class QAData(object):
 
         n_runs = len(run_length0)
 
-        if valid[0] is True:
+        if valid[0] == True:
             n_start = 1
         else:
             n_start = 0

Classes/TransectData.py
@@ -305,24 +305,28 @@ class TransectData(object):
 
             # Determine correct sign for latitude
             for n, lat_ref in enumerate(pd0_data.Gps2.lat_ref):
-                try:
-                    idx = [lat_ref.index('S')]
-                    if len(idx) > 0:
-                        raw_gga_lat[n, idx] = raw_gga_lat[n, idx] * -1
-                except ValueError:
-                    pass
+                idx = np.nonzero(np.array(lat_ref) == 'S')
+                raw_gga_lat[n, idx] = raw_gga_lat[n, idx] * -1
+                # try:
+                #     idx = [lat_ref.index('S')]
+                #     if len(idx) > 0:
+                #         raw_gga_lat[n, idx] = raw_gga_lat[n, idx] * -1
+                # except ValueError:
+                #     pass
             # idx = np.where(pd0_data.Gps2.lat_ref == 'S')[0]
             # if len(idx) > 0:
             #     raw_gga_lat[idx] = raw_gga_lat[idx] * -1
 
             # Determine correct sign for longitude
             for n, lon_ref in enumerate(pd0_data.Gps2.lon_ref):
-                try:
-                    idx = [lon_ref.index('W')]
-                    if len(idx) > 0:
-                        raw_gga_lon[n, idx] = raw_gga_lon[n, idx] * -1
-                except ValueError:
-                    pass
+                idx = np.nonzero(np.array(lon_ref) == 'W')
+                raw_gga_lon[n, idx] = raw_gga_lon[n, idx] * -1
+                # try:
+                #     idx = [lon_ref.index('W')]
+                #     if len(idx) > 0:
+                #         raw_gga_lon[n, idx] = raw_gga_lon[n, idx] * -1
+                # except ValueError:
+                #     pass
             # for sublist in data:
             #     if sublist[1] == 'W':
             #         print

MiscLibs/abba_2d_interpolation.py
@@ -343,26 +343,28 @@ def abba_idw_interpolation(data_list, valid_data, cells_above_sl, y_centers, y_c
     # Initialize output list
     interpolated_data = [[] for _ in range(len(data_list))]
 
-    # Find neighbors associated with each target
-    interpolation_points = find_neighbors(valid_data=valid_data,
-                                          cells_above_sl=cells_above_sl,
-                                          y_cell_centers=y_centers,
-                                          y_cell_size=y_cell_size,
-                                          y_normalize=y_normalize)
+    valid_cells = np.flatnonzero(valid_data[0, :])
+    if len(valid_cells) > 0:
+        # Find neighbors associated with each target
+        interpolation_points = find_neighbors(valid_data=valid_data,
+                                              cells_above_sl=cells_above_sl,
+                                              y_cell_centers=y_centers,
+                                              y_cell_size=y_cell_size,
+                                              y_normalize=y_normalize)
 
-    # Process each target
-    for point in interpolation_points:
-        # Compute distance from target to neighbors
-        distances = compute_distances(target=point['target'],
-                                      neighbors=point['neighbors'],
-                                      x=x_shiptrack,
-                                      y=y_centers)
+        # Process each target
+        for point in interpolation_points:
+            # Compute distance from target to neighbors
+            distances = compute_distances(target=point['target'],
+                                          neighbors=point['neighbors'],
+                                          x=x_shiptrack,
+                                          y=y_centers)
 
-        # Interpolate target for each data set in data_list
-        for n, data in enumerate(data_list):
-            interpolated_value = idw_interpolation(data=data,
-                                                   neighbor_indices=point['neighbors'],
-                                                   distances=distances)
-            interpolated_data[n].append([point['target'], interpolated_value])
+            # Interpolate target for each data set in data_list
+            for n, data in enumerate(data_list):
+                interpolated_value = idw_interpolation(data=data,
+                                                       neighbor_indices=point['neighbors'],
+                                                       distances=distances)
+                interpolated_data[n].append([point['target'], interpolated_value])
 
     return interpolated_data

UI/Qtmpl.py
@@ -355,21 +355,22 @@ class Qtmpl(FigureCanvas):
         # Set margins and padding for figure
         self.fig.subplots_adjust(left=0.2, bottom=0.15, right=0.98, top=0.98, wspace=0.1, hspace=0)
 
-        # Plot median values with error bars
-        self.extrap_plot_med(meas.extrap_fit.norm_data[-1])
+        if np.any(np.isnan(meas.extrap_fit.norm_data[-1].unit_normalized) == False):
+            # Plot median values with error bars
+            self.extrap_plot_med(meas.extrap_fit.norm_data[-1])
 
-        # Plot selected and automatic fits
-        self.extrap_plot_fit(meas.extrap_fit.sel_fit[-1])
+            # Plot selected and automatic fits
+            self.extrap_plot_fit(meas.extrap_fit.sel_fit[-1])
 
-        # Customize axis
-        self.fig.ax.set_xlabel(self.tr('Normalized Unit Q '))
-        self.fig.ax.set_ylabel(self.tr('Normalized Z '))
-        self.fig.ax.xaxis.label.set_fontsize(10)
-        self.fig.ax.yaxis.label.set_fontsize(10)
-        self.fig.ax.tick_params(axis='both', direction='in', bottom=True, top=True, left=True, right=True)
-        self.fig.ax.grid()
-        for label in (self.fig.ax.get_xticklabels() + self.fig.ax.get_yticklabels()):
-            label.set_fontsize(10)
+            # Customize axis
+            self.fig.ax.set_xlabel(self.tr('Normalized Unit Q '))
+            self.fig.ax.set_ylabel(self.tr('Normalized Z '))
+            self.fig.ax.xaxis.label.set_fontsize(10)
+            self.fig.ax.yaxis.label.set_fontsize(10)
+            self.fig.ax.tick_params(axis='both', direction='in', bottom=True, top=True, left=True, right=True)
+            self.fig.ax.grid()
+            for label in (self.fig.ax.get_xticklabels() + self.fig.ax.get_yticklabels()):
+                label.set_fontsize(10)
     #
     def extrap_plot_med(self, norm_data):
         """Plots median values and associated error bars.