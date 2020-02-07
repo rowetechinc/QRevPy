BoatData: 1) removed apply_filter call from sos_correction to speed computation as it is applied later thus it was duplicated DepthData: 1) fixed bug where the TRDI filter for multiples was attempting to be applied to the vertical beam, which is not a valid implementation, 2) fixed an assignment by reference that should have been a copy. Measurement: 1) removed unused parse import, 2) moved the reading of QRev.mat files to QRev.py and QRev.py passes the mat_data to Measurement. This allows QRev.py access to the version, 3) added the ability to force the abba interpolation when applying settings, 4) modified code so that when the transects selected for a measurement change the extrapolation is set to automatic, 5) fixed issue in xml output when no moving-bed test was selected, 6) fixed issue related to old QRev.mat file messages and sorting to get warnings on top Python2Matlab: 1) fixed issue with messages due to assignment by reference and changed to copy WaterData: 1) updated doc string to reflect abba, 2) removed apply_filter from apply_interpolation since it is called later to avoid duplication, 3) modified the code so that abba is called consistently from different locations QRev: 1) added import scipy.io to support reading Matlab file, 2) modified code to allow access to version from QRev.mat files, 3) removed call to config_gui() from save_measurement so tab colors remain active until a new file is loaded, 4) modified code to handle sorting of old QRev.mat meassages, 5) added code to clear the systest when accessing that tab, 6) modified code to properly show wait cursor when making changes in the tempsal table, 7) modified the moving-bed test table to only used the Used for Correction column if that moving-bed test is selected by the automated moving-bed selection algorithm, 8) fixed issue in WT table with highlighting SNR errors, 9) fixed issue with extrap tab not showing messages and comments when first opened.

Classes/DepthData.py
@@ -245,7 +245,7 @@ class DepthData(object):
         elif filter_type == 'Smooth':
             # R smooth filter
             self.filter_smooth(transect)
-        elif filter_type == 'TRDI':
+        elif filter_type == 'TRDI' and self.depth_source == 'BT':
             # TRDI filter for multiple returns
             self.filter_trdi()
             self.filter_type = 'TRDI'
@@ -489,7 +489,7 @@ class DepthData(object):
         # Determine number of beams
         if len(self.depth_orig_m.shape) > 1:
             n_beams, n_ensembles = self.depth_orig_m.shape[0], self.depth_orig_m.shape[1]
-            depth_raw = self.depth_orig_m
+            depth_raw = np.copy(self.depth_orig_m)
         else:
             n_beams = 1
             n_ensembles = self.depth_orig_m.shape[0]
@@ -740,7 +740,7 @@ class DepthData(object):
         """Filter used by TRDI to filter out multiple reflections that get digitized as depth."""
 
         # Assign raw depth data to local variable
-        depth_raw = copy.deepcopy(self.depth_orig_m)
+        depth_raw = np.copy(self.depth_orig_m)
 
         # Determine number of beams
         n_beams = depth_raw.shape[0]

Classes/Python2Matlab.py
@@ -212,7 +212,7 @@ class Python2Matlab(object):
         cell = np.zeros((1,), dtype=np.object)
         for n, line in enumerate(comments):
             cell[0] = line
-            struct[n] = cell
+            struct[n] = np.copy(cell)
         return struct
 
     @staticmethod

Classes/WaterData.py
@@ -101,9 +101,9 @@ class WaterData(object):
         wt_depth_filter: np.array(bool)
             WT in ensembles with invalid depths are marked invalid.
         interpolate_ens: str
-            Type of interpolation: "None", "TRDI", "Linear".
+            Type of interpolation: "None", "TRDI", "Linear", 'abba'.
         interpolate_cells: str
-            Type of cell interpolation: "None", "TRDI", "Linear"
+            Type of cell interpolation: "None", "TRDI", "Linear", 'abba'
         coord_sys: str
             Defines the velocity coordinate system "Beam", "Inst", "Ship", "Earth"
         nav_ref: str
@@ -860,7 +860,7 @@ class WaterData(object):
         self.v_earth_no_ref_mps = self.v_earth_no_ref_mps * ratio
 
         # Apply filters to new water velocities
-        self.apply_filter(transect)   
+        # self.apply_filter(transect)
         
     def adjust_side_lobe(self, transect):
         """Adjust the side lobe cutoff for vertical beam and interpolated depths.
@@ -997,33 +997,44 @@ class WaterData(object):
 
         # Check for presence of 3-beam solutions
         if len(rows_3b) > 0:
+            interpolated_data = self.compute_abba_interpolation(wt_data=temp,
+                                                                valid=temp.valid_data[5, :, :],
+                                                                transect=transect)
+            # # Find cells with invalid data
+            # valid_cells = np.equal(self.valid_data[0, :, :].astype(int), self.valid_data[6, :, :].astype(int)).astype(
+            #     bool)
+            # boat_selected = getattr(transect.boat_vel, transect.boat_vel.selected)
+            # boat_valid = boat_selected.valid_data[0]
+            #
+            # if not np.all(valid_cells) and np.nansum(boat_valid) > 1:
+            #     # Data needed for interpolation
+            #     distance_along_shiptrack = transect.boat_vel.compute_boat_track(transect)['distance_m']
+            #     depth_selected = getattr(transect.depths, transect.depths.selected)
+            #
+            #     # Interpolate values for cells with 3-beam solutions from neighboring data
+            #     interpolated_data = abba_idw_interpolation(data_list=[temp.u_mps, temp.v_mps],
+            #                                                valid_data=temp.valid_data[5, :, :],
+            #                                                cells_above_sl=temp.cells_above_sl,
+            #                                                y_centers=depth_selected.depth_cell_depth_m,
+            #                                                y_cell_size=depth_selected.depth_cell_size_m,
+            #                                                y_depth=depth_selected.depth_processed_m,
+            #                                                x_shiptrack=distance_along_shiptrack,
+            #                                                normalize=True)
+            if interpolated_data is not None:
+                # Compute interpolated to measured ratios and apply filter criteria
+                for n in range(len(interpolated_data[0])):
+                    u_ratio = (temp.u_mps[interpolated_data[0][n][0]] / interpolated_data[0][n][1]) - 1
+                    v_ratio = (temp.v_mps[interpolated_data[1][n][0]] / interpolated_data[1][n][1]) - 1
+                    if np.abs(u_ratio) < 0.5 and np.abs(v_ratio) < 0.5:
+                        valid_bool[interpolated_data[0][n][0]] = True
+                    else:
+                        valid_bool[interpolated_data[0][n][0]] = False
+                    # n += 1
 
-            # Data needed for interpolation
-            distance_along_shiptrack = transect.boat_vel.compute_boat_track(transect)['distance_m']
-            depth_selected = getattr(transect.depths, transect.depths.selected)
-
-            # Interpolate values for cells with 3-beam solutions from neighboring data
-            interpolated_data = abba_idw_interpolation(data_list=[temp.u_mps, temp.v_mps],
-                                                       valid_data=temp.valid_data[5, :, :],
-                                                       cells_above_sl=temp.cells_above_sl,
-                                                       y_centers=depth_selected.depth_cell_depth_m,
-                                                       y_cell_size=depth_selected.depth_cell_size_m,
-                                                       y_depth=depth_selected.depth_processed_m,
-                                                       x_shiptrack=distance_along_shiptrack,
-                                                       normalize=True)
-
-            # Compute interpolated to measured ratios and apply filter criteria
-            for n in range(len(interpolated_data[0])):
-                u_ratio = (temp.u_mps[interpolated_data[0][n][0]] / interpolated_data[0][n][1]) - 1
-                v_ratio = (temp.v_mps[interpolated_data[1][n][0]] / interpolated_data[1][n][1]) - 1
-                if np.abs(u_ratio) < 0.5 and np.abs(v_ratio) < 0.5:
-                    valid_bool[interpolated_data[0][n][0]] = True
-                else:
-                    valid_bool[interpolated_data[0][n][0]] = False
-                # n += 1
-
-            # Update object with filter results
-            self.valid_data[5, :, :] = valid_bool
+                # Update object with filter results
+                self.valid_data[5, :, :] = valid_bool
+            else:
+                self.valid_data[5, :, :] = temp.valid_data[5, :, :]
         else:
             self.valid_data[5, :, :] = temp.valid_data[5, :, :]
 
@@ -1390,8 +1401,47 @@ class WaterData(object):
         self.u_processed_mps[np.logical_not(valid)] = np.nan
         self.v_processed_mps[np.logical_not(valid)] = np.nan
 
+        interpolated_data = self.compute_abba_interpolation(wt_data=self,valid=valid, transect=transect)
+        # # Find cells with invalid data
+        # valid_cells = np.equal(self.valid_data[0, :, :].astype(int), self.valid_data[6, :, :].astype(int)).astype(bool)
+        # boat_selected = getattr(transect.boat_vel, transect.boat_vel.selected)
+        # boat_valid = boat_selected.valid_data[0]
+        #
+        # if not np.all(valid_cells) and np.nansum(boat_valid) > 1:
+        #     # Compute distance along shiptrack to be used in interpolation
+        #     distance_along_shiptrack = transect.boat_vel.compute_boat_track(transect)['distance_m']
+        #
+        #     # Where there is invalid boat speed at beginning or end of transect mark the distance nan to avoid
+        #     # interpolating velocities that won't be used for discharge
+        #
+        #     distance_along_shiptrack[0:np.argmax(boat_valid==True)] = np.nan
+        #     end_nan = np.argmax(np.flip(boat_valid) == True)
+        #     if end_nan > 0:
+        #         distance_along_shiptrack[-1*end_nan:] = np. nan
+        #     if type(distance_along_shiptrack) is np.ndarray:
+        #         depth_selected = getattr(transect.depths, transect.depths.selected)
+        #
+        #         # Interpolate values for  invalid cells with from neighboring data
+        #         interpolated_data = abba_idw_interpolation(data_list=[self.u_processed_mps, self.v_processed_mps],
+        #                                                    valid_data=valid,
+        #                                                    cells_above_sl=self.valid_data[6, :, :],
+        #                                                    y_centers=depth_selected.depth_cell_depth_m,
+        #                                                    y_cell_size=depth_selected.depth_cell_size_m,
+        #                                                    y_depth=depth_selected.depth_processed_m,
+        #                                                    x_shiptrack=distance_along_shiptrack,
+        #                                                    normalize=True)
+        if interpolated_data is not None:
+            # Incorporate interpolated values in processed data
+            for n in range(len(interpolated_data[0])):
+                self.u_processed_mps[interpolated_data[0][n][0]] = \
+                    interpolated_data[0][n][1]
+                self.v_processed_mps[interpolated_data[1][n][0]] = \
+                    interpolated_data[1][n][1]
+
+    @staticmethod
+    def compute_abba_interpolation(wt_data, valid, transect):
         # Find cells with invalid data
-        valid_cells = np.equal(self.valid_data[0, :, :].astype(int), self.valid_data[6, :, :].astype(int)).astype(bool)
+        valid_cells = np.equal(wt_data.valid_data[0, :, :].astype(int), wt_data.valid_data[6, :, :].astype(int)).astype(bool)
         boat_selected = getattr(transect.boat_vel, transect.boat_vel.selected)
         boat_valid = boat_selected.valid_data[0]
 
@@ -1402,30 +1452,25 @@ class WaterData(object):
             # Where there is invalid boat speed at beginning or end of transect mark the distance nan to avoid
             # interpolating velocities that won't be used for discharge
 
-            distance_along_shiptrack[0:np.argmax(boat_valid==True)] = np.nan
+            distance_along_shiptrack[0:np.argmax(boat_valid == True)] = np.nan
             end_nan = np.argmax(np.flip(boat_valid) == True)
             if end_nan > 0:
-                distance_along_shiptrack[-1*end_nan:] = np. nan
+                distance_along_shiptrack[-1 * end_nan:] = np.nan
             if type(distance_along_shiptrack) is np.ndarray:
                 depth_selected = getattr(transect.depths, transect.depths.selected)
 
                 # Interpolate values for  invalid cells with from neighboring data
-                interpolated_data = abba_idw_interpolation(data_list=[self.u_processed_mps, self.v_processed_mps],
+                interpolated_data = abba_idw_interpolation(data_list=[wt_data.u_processed_mps, wt_data.v_processed_mps],
                                                            valid_data=valid,
-                                                           cells_above_sl=self.valid_data[6, :, :],
+                                                           cells_above_sl=wt_data.valid_data[6, :, :],
                                                            y_centers=depth_selected.depth_cell_depth_m,
                                                            y_cell_size=depth_selected.depth_cell_size_m,
                                                            y_depth=depth_selected.depth_processed_m,
                                                            x_shiptrack=distance_along_shiptrack,
                                                            normalize=True)
-
-                # Incorporate interpolated values in processed data
-                for n in range(len(interpolated_data[0])):
-                    self.u_processed_mps[interpolated_data[0][n][0]] = \
-                        interpolated_data[0][n][1]
-                    self.v_processed_mps[interpolated_data[1][n][0]] = \
-                        interpolated_data[1][n][1]
-
+            return interpolated_data
+        else:
+            return None
     def interpolate_ens_next(self):
         """Applies data from the next valid ensemble for ensembles with invalid water velocities."""
 

UI/QRev.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import pyqtSignal, QRegExp
 import numpy as np
+import scipy.io as sio
 import sys
 import threading
 import copy
@@ -206,16 +207,18 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
 
         # If a selection is made begin loading
         if len(select.type) > 0:
-            with self.wait_cursor():
-                # Load and process Sontek data
-                if select.type == 'SonTek':
+
+            # Load and process Sontek data
+            if select.type == 'SonTek':
+                with self.wait_cursor():
                     # Show folder name in GUI header
                     self.setWindowTitle(self.QRev_version + ': ' + select.pathName)
                     # Create measurement object
                     self.meas = Measurement(in_file=select.fullName, source='SonTek', proc_type='QRev')
 
-                # Load and process TRDI data
-                elif select.type == 'TRDI':
+            # Load and process TRDI data
+            elif select.type == 'TRDI':
+                with self.wait_cursor():
                     # Show mmt filename in GUI header
                     self.setWindowTitle(self.QRev_version + ': ' + select.fullName[0])
                     # Create measurement object
@@ -224,14 +227,32 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
                                             proc_type='QRev',
                                             checked=select.checked)
 
-                # Load QRev data
-                elif select.type == 'QRev':
-                    # Show QRev filename in GUI header
-                    self.setWindowTitle(self.QRev_version + ': ' + select.fullName)
-                    self.meas = Measurement(in_file=select.fullName, source='QRev')
-                else:
-                    print('Cancel')
+            # Load QRev data
+            elif select.type == 'QRev':
+                # Show QRev filename in GUI header
+                self.setWindowTitle(self.QRev_version + ': ' + select.fullName)
+                mat_data = sio.loadmat(select.fullName,
+                            struct_as_record=False,
+                            squeeze_me=True)
 
+                if not self.QRev_version == mat_data['version']:
+                    message = 'QRev has been updated (version ' + self.QRev_version + \
+                              ') since this file was saved (version ' + mat_data['version'] + \
+                              '). You can view the file without reprocessing by pressing No. ' + \
+                              'NOTE: Any changes will reprocess the file using the QRev updates. ' + \
+                              'You can reprocess the file now by pressing Yes.'
+                    response = QtWidgets.QMessageBox.question(self, 'Reprocess', message,
+                                                              QtWidgets.QMessageBox.No | QtWidgets.QMessageBox.Yes,
+                                                              QtWidgets.QMessageBox.No)
+                    with self.wait_cursor():
+                        if response == QtWidgets.QMessageBox.No:
+                            self.meas = Measurement(in_file=mat_data, source='QRev', proc_type=None)
+                        else:
+                            self.meas = Measurement(in_file=mat_data, source='QRev', proc_type='QRev')
+            else:
+                print('Cancel')
+
+            with self.wait_cursor():
                 # Identify transects to be used in discharge computation
                 self.checked_transects_idx = Measurement.checked_transects(self.meas)
                 
@@ -265,7 +286,7 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
                 Python2Matlab.save_matlab_file(self.meas, save_file.full_Name)
             else:
                 Python2Matlab.save_matlab_file(self.meas, save_file.full_Name, checked=self.checked_transects_idx)
-            self.config_gui()
+            # self.config_gui()
             self.meas.xml_output(self.QRev_version, save_file.full_Name[:-4] + '.xml')
 
             # Save stylesheet in measurement folder
@@ -1001,7 +1022,13 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
             qa_type = getattr(qa, key)
             if qa_type['messages']:
                 for message in qa_type['messages']:
-                    messages.append(message)
+                    if type(message) is str:
+                        if message[:3].isupper():
+                            messages.append([message, 1])
+                        else:
+                            messages.append([message, 2])
+                    else:
+                        messages.append(message)
             self.setIcon(key, qa_type['status'])
 
         # Sort messages with warning at top
@@ -1023,7 +1050,7 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
         for row, message in enumerate(messages):
             # Handle messages from old QRev that did not have integer codes
             if type(message) is str:
-                warn = message[:message.find(':')].isupper()
+                warn = message[:3].isupper()
                 tbl.setItem(row, 1, QtWidgets.QTableWidgetItem(message))
             # Handle newer style messages
             else:
@@ -1889,6 +1916,7 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
         tbl.horizontalHeader().setFont(self.font_bold)
         tbl.verticalHeader().hide()
         tbl.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
+        self.display_systest.clear()
 
         # Add system tests
         if nrows > 0:
@@ -2877,28 +2905,29 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
             t_source_entered = t_source_dialog.exec_()
 
             if t_source_entered:
-                # Assign data based on change made by user
-                old_discharge = copy.deepcopy(self.meas.discharge)
-                if t_source_dialog.rb_internal.isChecked():
-                    t_source = 'internal'
-                elif t_source_dialog.rb_user.isChecked():
-                    t_source = 'user'
-                    user_temp = float(t_source_dialog.ed_user_temp.text())
+                with self.wait_cursor():
+                    # Assign data based on change made by user
+                    old_discharge = copy.deepcopy(self.meas.discharge)
+                    if t_source_dialog.rb_internal.isChecked():
+                        t_source = 'internal'
+                    elif t_source_dialog.rb_user.isChecked():
+                        t_source = 'user'
+                        user_temp = float(t_source_dialog.ed_user_temp.text())
 
-                # Apply change to all or only selected transect based on user input
-                if t_source_dialog.rb_all.isChecked():
-                    self.meas.change_sos(parameter='temperatureSrc',
-                                         temperature=user_temp,
-                                         selected=t_source)
-                else:
-                    self.meas.change_sos(transect_idx=self.checked_transects_idx[row],
-                                         parameter='temperatureSrc',
-                                         temperature=user_temp,
-                                         selected=t_source)
+                    # Apply change to all or only selected transect based on user input
+                    if t_source_dialog.rb_all.isChecked():
+                        self.meas.change_sos(parameter='temperatureSrc',
+                                             temperature=user_temp,
+                                             selected=t_source)
+                    else:
+                        self.meas.change_sos(transect_idx=self.checked_transects_idx[row],
+                                             parameter='temperatureSrc',
+                                             temperature=user_temp,
+                                             selected=t_source)
 
-                # Update the tempsal tab
-                self.update_tempsal_tab(tbl=tbl, old_discharge=old_discharge, new_discharge=self.meas.discharge)
-                self.change = True
+                    # Update the tempsal tab
+                    self.update_tempsal_tab(tbl=tbl, old_discharge=old_discharge, new_discharge=self.meas.discharge)
+                    self.change = True
 
         # Change salinity
         elif column == 3:
@@ -2908,21 +2937,23 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
             salinity_entered = salinity_dialog.exec_()
 
             if salinity_entered:
-                # Assign data based on change made by user
-                old_discharge = copy.deepcopy(self.meas.discharge)
-                salinity = float(salinity_dialog.ed_salinity.text())
+                with self.wait_cursor():
+                    # Assign data based on change made by user
+                    old_discharge = copy.deepcopy(self.meas.discharge)
+                    salinity = float(salinity_dialog.ed_salinity.text())
 
-                # Apply change to all or only selected transect based on user input
-                if salinity_dialog.rb_all.isChecked():
-                    self.meas.change_sos(parameter='salinity',
-                                         salinity=salinity)
-                else:
-                    self.meas.change_sos(transect_idx=self.checked_transects_idx[row],
-                                         parameter='salinity',
-                                         salinity=salinity)
-                # Update the tempsal tab
-                self.update_tempsal_tab(tbl=tbl, old_discharge=old_discharge, new_discharge=self.meas.discharge)
-                self.change = True
+                    # Apply change to all or only selected transect based on user input
+                    if salinity_dialog.rb_all.isChecked():
+                        self.meas.change_sos(parameter='salinity',
+                                             salinity=salinity)
+                    else:
+                        self.meas.change_sos(transect_idx=self.checked_transects_idx[row],
+                                             parameter='salinity',
+                                             salinity=salinity)
+                    # Update the tempsal tab
+                    self.update_tempsal_tab(tbl=tbl, old_discharge=old_discharge, new_discharge=self.meas.discharge)
+                    self.change = True
+
         # Change speed of sound
         elif column == 4:
             user_sos = None
@@ -2938,28 +2969,29 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
             sos_source_entered = sos_source_dialog.exec_()
 
             if sos_source_entered:
-                # Assign data based on change made by user
-                old_discharge = copy.deepcopy(self.meas.discharge)
-                if sos_source_dialog.rb_internal.isChecked():
-                    sos_source = 'internal'
-                elif sos_source_dialog.rb_user.isChecked():
-                    sos_source = 'user'
-                    user_sos = float(sos_source_dialog.ed_sos_user.text()) / self.units['V']
+                with self.wait_cursor():
+                    # Assign data based on change made by user
+                    old_discharge = copy.deepcopy(self.meas.discharge)
+                    if sos_source_dialog.rb_internal.isChecked():
+                        sos_source = 'internal'
+                    elif sos_source_dialog.rb_user.isChecked():
+                        sos_source = 'user'
+                        user_sos = float(sos_source_dialog.ed_sos_user.text()) / self.units['V']
 
-                # Apply change to all or only selected transect based on user input
-                if sos_source_dialog.rb_all.isChecked():
-                    self.meas.change_sos(parameter='sosSrc',
-                                         speed=user_sos,
-                                         selected=sos_source)
-                else:
-                    self.meas.change_sos(transect_idx=self.checked_transects_idx[row],
-                                         parameter='sosSrc',
-                                         speed=user_sos,
-                                         selected=sos_source)
+                    # Apply change to all or only selected transect based on user input
+                    if sos_source_dialog.rb_all.isChecked():
+                        self.meas.change_sos(parameter='sosSrc',
+                                             speed=user_sos,
+                                             selected=sos_source)
+                    else:
+                        self.meas.change_sos(transect_idx=self.checked_transects_idx[row],
+                                             parameter='sosSrc',
+                                             speed=user_sos,
+                                             selected=sos_source)
 
-                # Update the tempsal tab
-                self.update_tempsal_tab(tbl=tbl, old_discharge=old_discharge, new_discharge=self.meas.discharge)
-                self.change = True
+                    # Update the tempsal tab
+                    self.update_tempsal_tab(tbl=tbl, old_discharge=old_discharge, new_discharge=self.meas.discharge)
+                    self.change = True
 
     def tempsal_comments_messages(self):
         """Displays comments and messages associated with temperature, salinity, and speed of sound in Messages tab.
@@ -3196,10 +3228,10 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
                 checked2.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
                 tbl.setItem(row, col, QtWidgets.QTableWidgetItem(checked2))
                 tbl.item(row, col).setFlags(QtCore.Qt.ItemIsEnabled)
+                tbl.item(row, col).setCheckState(QtCore.Qt.Unchecked)
                 if self.meas.mb_tests[row].use_2_correct:
-                    tbl.item(row, col).setCheckState(QtCore.Qt.Checked)
-                else:
-                    tbl.item(row, col).setCheckState(QtCore.Qt.Unchecked)
+                    if self.meas.mb_tests[row].selected:
+                        tbl.item(row, col).setCheckState(QtCore.Qt.Checked)
 
                 # Filename
                 col += 1
@@ -6077,16 +6109,19 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
                 item = '{:3.2f}'.format((num_snr_invalid / num_useable_cells) * 100.)
                 tbl.setItem(row, col, QtWidgets.QTableWidgetItem(item))
                 tbl.item(row, col).setFlags(QtCore.Qt.ItemIsEnabled)
-                if self.meas.qa.w_vel['q_total_warning'][transect_id, 6] or \
-                        self.meas.qa.w_vel['q_max_run_warning'][transect_id, 6]:
+                if self.meas.qa.w_vel['q_total_warning'].shape[1] > 7:
+                    if self.meas.qa.w_vel['q_total_warning'][transect_id, 7] or \
+                            self.meas.qa.w_vel['q_max_run_warning'][transect_id, 7]:
 
-                    tbl.item(row, col).setBackground(QtGui.QColor(255, 77, 77))
+                        tbl.item(row, col).setBackground(QtGui.QColor(255, 77, 77))
 
-                elif self.meas.qa.w_vel['q_total_caution'][transect_id, 6] or \
-                        self.meas.qa.w_vel['q_max_run_caution'][transect_id, 6]:
+                    elif self.meas.qa.w_vel['q_total_caution'][transect_id, 7] or \
+                            self.meas.qa.w_vel['q_max_run_caution'][transect_id, 7]:
 
-                    tbl.item(row, col).setBackground(QtGui.QColor(255, 204, 0))
+                        tbl.item(row, col).setBackground(QtGui.QColor(255, 204, 0))
 
+                    else:
+                        tbl.item(row, col).setBackground(QtGui.QColor(255, 255, 255))
                 else:
                     tbl.item(row, col).setBackground(QtGui.QColor(255, 255, 255))
 
@@ -6588,6 +6623,8 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
 
         self.extrap_set_data()
         self.extrap_plot()
+
+        self.extrap_comments_messages()
 
         if not self.extrap_initialized:
             self.table_extrap_qsen.cellClicked.connect(self.sensitivity_change_fit)
@@ -8321,9 +8358,9 @@ class QRev(QtWidgets.QMainWindow, QRev_gui.Ui_MainWindow):
             # set the change status to True for the main window update
             self.change = True
 
-            # Set Extrap combo boxes back to auto then force an extrap update
-            self.combo_extrap_fit.setCurrentIndex(0)
-            self.combo_extrap_data.setCurrentIndex(0)
+            # # Set Extrap combo boxes back to auto then force an extrap update
+            # self.combo_extrap_fit.setCurrentIndex(0)
+            # self.combo_extrap_data.setCurrentIndex(0)
 
         self.update_main()
 