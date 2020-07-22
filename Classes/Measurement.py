804c343b42134ca5db63d6fb61f9db5b539fb547
dmueller <dmueller@usgs.gov>
2020-07-22 09:31:26 -0400

    Commented out calls to Oursin to allow other code changes to be merged with master

--- Classes/Measurement.py
+++ Classes/Measurement.py
@@ -160,11 +160,11 @@ class Measurement(object):
                 self.uncertainty.compute_uncertainty(self)
 
                 self.qa = QAData(self)
-                self.oursin = Oursin()
-                self.oursin.compute_oursin(self)
-
-                self.oursin_orig = Oursin_orig()
-                self.oursin_orig.compute_oursin(self)
+                # self.oursin = Oursin()
+                # self.oursin.compute_oursin(self)
+                #
+                # self.oursin_orig = Oursin_orig()
+                # self.oursin_orig.compute_oursin(self)
 
     def load_trdi(self, mmt_file, transect_type='Q', checked=False):
         """Method to load TRDI data.
